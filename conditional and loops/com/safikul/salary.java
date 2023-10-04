7454b381902d484352e234a161d87bf55d16060c
safikulalam96 <safikulalam9679@gmail.com>
2023-10-04 23:09:32 +0530

    salary bonus

---
+++ conditional and loops/com/safikul/salary.java
@@ -0,0 +1,19 @@
+/* Calculating the bonus based on salary */
+package com.safikul;
+import java.util.Scanner;
+public class salary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the your salary to get the bonus ");
+        float salary = input.nextFloat();
+        if (salary <= 10000) {
+            salary += 1000;
+        } else if (salary > 10001 && salary < 20000) {
+            salary += 2500;
+        } else {
+            salary += 4000;
+        }
+        System.out.print("Your new salary is : " + salary);
+    }
+}
+
