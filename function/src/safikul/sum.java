c282fda213308332b12e460957efef298ddb3339
safikulalam96 <safikulalam9679@gmail.com>
2023-10-07 02:10:50 +0530

    function sum

---
+++ function/src/safikul/sum.java
@@ -0,0 +1,18 @@
+package src.safikul;
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        addition();
+    }
+    static  void addition(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the first no ");
+        int a=input.nextInt();
+        System.out.println("Enter the second no ");
+        int b=input.nextInt();
+        int c=a+b;
+        System.out.println("The sum of the two no is :  "+ c);
+
+    }
+}
+
