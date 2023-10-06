834d0eb4a66137632a670a47de8522160677c04f
safikulalam96 <safikulalam9679@gmail.com>
2023-10-07 02:33:44 +0530

    learning how to return a function

---
+++ function/src/safikul/return_string.java
@@ -0,0 +1,15 @@
+package src.safikul;
+import java.util.Scanner;
+public class return_string {
+    public static void main(String[] args) {
+        String mssg=greet();
+        System.out.print(mssg);
+    }
+    static String greet(){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the message to print ");
+        String mms=in.nextLine();
+        return mms;
+
+    }
+}
