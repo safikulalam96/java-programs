9247a56b1fa92a6c40e5d9835b3bfdf5e75b0715
safikulalam96 <safikulalam9679@gmail.com>
2023-10-09 01:10:36 +0530

    non primitive data type

---
+++ function/src/safikul/non_primitive.java
@@ -0,0 +1,16 @@
+package src.safikul;
+import java.util.Arrays;
+import java.util.Scanner;
+public class non_primitive {
+    public static void main(String[] args) {
+        int[] arr={23,34,33,223,90};
+        System.out.println("Before execution of the problem");
+        System.out.println(Arrays.toString(arr));
+        simple(arr);
+        System.out.println("After execution of the problem");
+        System.out.println(Arrays.toString(arr));
+    }
+    static void simple(int[] num){
+        num[0]=2;
+    }
+}
