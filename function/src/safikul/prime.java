35fa136dbf3dc7ac8f3f2e39e92647737c0164d4
safikulalam96 <safikulalam9679@gmail.com>
2023-10-10 12:42:24 +0530

    prime number checking

---
+++ function/src/safikul/prime.java
@@ -0,0 +1,27 @@
+package src.safikul;
+import java.util.Scanner;
+public class prime {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the number to check prime or not ");
+        int no= in.nextInt();
+
+        check(no);
+
+    }
+    static void  check(int pr){
+        int i=1;
+        int count=0;
+        while(i<=pr){
+            if(pr%i==0){
+                count++;
+            }
+            i++;
+        }
+        if(count>2){
+            System.out.println("Your number is a composite number");
+        }
+        else
+            System.out.println("Your number is a prime number");
+    }
+}
